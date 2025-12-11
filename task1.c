f8095b234ef0be4c0617dd81e739fc03f07c207e
ibraryounis <ibraryounis027@gmail.com>
2025-12-11 16:43:43 +0500

    updated code

--- task1.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    printf("C-PROGRAMMING-ZERO-TO-HERO");
-    return 0;
-}
--- week01-basics/day01-hello-world/task1.c
+++ week01-basics/day01-hello-world/task1.c
@@ -2,5 +2,6 @@
 int main()
 {
     printf("C-PROGRAMMING-ZERO-TO-HERO");
+    printf("C-PROGRAMMING-ZERO-TO-HERO");
     return 0;
 }
